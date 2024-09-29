b347bbf5c59459da736075998085a34954647694
Alexander Evdokimov <83905079+snakerzr@users.noreply.github.com>
2024-09-29 07:27:08 +0300

    new functionality

--- api/api.py
+++ api/api.py
@@ -1,10 +1,13 @@
+import re
+
 import uvicorn
 from fastapi import FastAPI
 
+from src.classifier import id2label_one, id2label_two, onnx_clf_one, onnx_clf_two
 from src.rag.main import rag_pipeline
 from src.rag.retriever import create_chroma_db, get_collection
-from src.classifier import onnx_clf_one, onnx_clf_two, id2label_one, id2label_two
-from .schema import Request, Response
+
+from .schema import Request, Response, ResponseClassify
 
 create_chroma_db()
 
@@ -19,28 +22,35 @@ def index():
 
 @app.post("/predict")
 async def get_reponse(request: Request) -> Response:
-    result_one = onnx_clf_one(request.question)
-    result_two = onnx_clf_two(request.question)
+    answer = rag_pipeline(request.question)['actual_response']
+    if re.findall('не знаю|не могу|нет ответа', answer.lower()):
+        class_1 = 'None'
+        class_2 = 'None'
+    else:
+        result_one = onnx_clf_one(request.question)
+        class_1 = id2label_one[result_one[0]['label'].split('_')[1]]
+        result_two = onnx_clf_two(request.question)
+        class_2 = id2label_two[result_two[0]['label'].split('_')[1]]
 
     response = Response(
-        answer=rag_pipeline(input=request.question,collection=collection)["actual_response"],
-        class_1=id2label_one[result_one[0]["label"].split("_")[1]],
-        class_2=id2label_two[result_two[0]["label"].split("_")[1]],
+        answer=answer,
+        class_1=class_1,
+        class_2=class_2
     )
     return response
 
 
 @app.post("/classify")
-async def predict_class(request: Request) -> Response:
+async def predict_class(request: Request) -> ResponseClassify:
     result_one = onnx_clf_one(request.question)
+    class_1=id2label_one[result_one[0]['label'].split('_')[1]]
     result_two = onnx_clf_two(request.question)
+    class_2=id2label_two[result_two[0]['label'].split('_')[1]]
 
-    response = Response(
-        answer="", 
-        class_1=id2label_one[result_one[0]["label"].split("_")[1]],
-        class_2=id2label_two[result_two[0]["label"].split("_")[1]],
-    )
-    return response
+    return {'class_1': class_1,
+     'answer_1': result_one[0],
+     'class_2': class_2,
+     'answer_2': result_two[0]}
 
 
 if __name__ == "__main__":
--- api/schema.py
+++ api/schema.py
@@ -9,3 +9,9 @@ class Response(BaseModel):
     answer: str
     class_1: str
     class_2: str
+
+class ResponseClassify(BaseModel):
+    answer_1: float
+    class_1: str
+    answer_2: float
+    class_2: str
